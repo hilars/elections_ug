c1becce277fa898997ec338f27492387c384a47d
hilars <hilkansiime@gmail.com>
2022-01-28 13:13:06 +0300

    Update ind.js

--- ind.js
+++ ind.js
@@ -28,16 +28,28 @@ var margin = {top: 30, right: 30, bottom: 70, left: 60},
        )
        
        //Add constituency to drop down 
-        d3.select("#districtmp").selectAll("option")
+       /* d3.select("#districtmp").selectAll("option")
             .data(d3.map(dat, (d) => {
                  return d}))
             .enter()
             .append("option")
             .text(function(d){return d;})
-            .attr("value", function(d){return d;})
-
+            .attr("value", function(d){return d;})*/
+        
+            district1 = d3.group(data, d => d.District);
+            //var selecti = d3.select("#districtmp").property("value")
+            
                 
-        bars(data);
+        bars(district1.get(d3.select("#districtcorr").property("value")));
+
+        $("#districtcorr").on("change", (e) => {
+            del_it();
+            //const val = data.find(el => el. == stations.get(e.target.value));
+            district1 = d3.group(data, d => d.District);
+            //var selecti = d3.select("#districtmp").property("value")
+            
+            bars(district1.get(d3.select("#districtcorr").property("value")));
+        })
                 
 
     }
@@ -45,11 +57,10 @@ var margin = {top: 30, right: 30, bottom: 70, left: 60},
 
 
     function bars(clients){
-        district1 = d3.group(clients, d => d.District);
-        var selecti = d3.select("#districtmp").property("value")
+        
               //write(selecti)
         
-        let datafilt = district1.get(selecti)
+        let datafilt = clients
 
         let maxi = d3.max(datafilt, function(d) { return d.Votes; } );
         let sum = d3.sum(datafilt, function(d) { return d.Votes; } );
@@ -87,8 +98,8 @@ var margin = {top: 30, right: 30, bottom: 70, left: 60},
         let xAxis = d3.axisBottom(widthScale)
                     .ticks(5)
                     .tickFormat(d => d + "%" )
-        d3.select("#xAxis")
-            .attr("transform", "translate(100, 300)")
+        xs = d3.select("#xAxis")
+            .attr("transform", "translate(100,  300)")
             .call(xAxis)
         let yAxis = d3.axisLeft(positionScale)
         ys = d3.select("#yAxis")
@@ -96,45 +107,7 @@ var margin = {top: 30, right: 30, bottom: 70, left: 60},
             .call(yAxis)
 
 
-            function update(selectedGroup) {
-
-                // Create new data with the selection?
-                //const dataFilter = data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
-                
-                
-                //var dataFilter = dat.filter(function(d){return d.Votes==maxi})
-                // Give these new data to update line
-                console.log(selectedGroup)
-                let positionScale = d3.scaleBand()
-                            .range([0, 200])
-                            .domain(selectedGroup.map(d => d.PoliticalParty))
-                            .padding(0.3)
-                ys.call(yAxis)
-                cov .append("rect")
-                            .style("stroke","white")
-                            .attr("fill", function (d) { return colorScale(d.PoliticalParty)})
-                            .attr("width", d => widthScale((d.Votes/sum)*100))
-                            .attr("height", positionScale.bandwidth())
-                            .attr("y", d => positionScale(d.PoliticalParty))
-                
-                /*cov
-                  .data(selectedGroup)
-                  
-                  .transition()
-                  .duration(1000)
-                    .attr("cx", d => xScale(d.winning_percent))
-                    .attr("cy", d => yScale(d.voter_turnup))*/
-              }
             
-              d3.select("#districtmp").on("change", function(event, d) {
-                // recover the option that has been chosen
-                let selectedOption = d3.select(this).property("value")
-                district2 = d3.group(clients, d => d.District);
-                let dat11 = district2.get(selectedOption)
-                // run the updateChart function with this selected option
-                console.log("h'"+dat11)
-                update(dat11)
-            })
 
 
     }
@@ -144,5 +117,5 @@ var margin = {top: 30, right: 30, bottom: 70, left: 60},
         container.append("div").text(text)
     }
     function del_it(){
-        container.selectAll("div").remove()
+        container.selectAll("rect").remove()
     }
